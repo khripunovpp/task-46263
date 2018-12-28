7f0b5d86acdb6cc6bc9e4e1a27448e50493acfa1
Павел Хрипунов <khripunovpp@gmail.com>
2018-12-28 16:54:51 +0300

    add form

--- app/js/common.js
+++ app/js/common.js
@@ -11,6 +11,22 @@ var app = function() {
         });
     });
 
+    $('a').click(function(e) {
+        e.preventDefault();
+        var a = $('.js_submit');
+        var b = a.closest('form');
+
+        if ($('form#toform').length) {
+            a = $('#toform .js_submit');
+            b = a.closest('form#toform');
+        }
+
+        if (b.length && a.is(':visible')) {
+            $("html,body").animate({ scrollTop: b.last().offset().top }, 1000);
+        }
+        return false;
+    });
+
 }
 
 $(function() {
